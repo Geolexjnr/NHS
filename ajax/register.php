84b3b22bff2c9419fdba756688ca67f312075c1d
George Chileya <george.chileya@outlook.com>
2021-07-20 04:13:45 +0200

    second commit

--- ajax/register.php
+++ ajax/register.php
@@ -6,7 +6,7 @@
  if(isset($_POST['username'])){
     $name=mysqli_real_escape_string($con,$_POST['name']);
      $username=mysqli_real_escape_string($con,$_POST['username']);
-     $email=mysqli_real_escape_string($con,$_POST['username']);
+     $email=mysqli_real_escape_string($con,$_POST['email']);
      $referral_username=mysqli_real_escape_string($con,$_POST['referral_username']);
      $mobile=mysqli_real_escape_string($con,$_POST['mobile']);
      $address=mysqli_real_escape_string($con,$_POST['address']);
@@ -22,13 +22,29 @@
      $converted = $fetched_id['id'];
 
      $reg_date = date("Y-m-d h:i:s");
+
+     $flag_check = false;
+
+
+     $query_check_info = mysqli_query($con, "SELECT `id` from user where username='$username' 
+     or email = '$email' or mobile = '$mobile' limit 1" );
+
+
     //insert into the user table
-     $sql = "INSERT INTO user (username, name, email, mobile, address, referral_user, join_date)
+     $sql = "INSERT INTO user (`username`, `name`, `email`, `mobile`, `address`, `referral_user`, `join_date`)
      	VALUES ('$username', '$name', '$email',  '$mobile', '$address', $converted, '$reg_date')";
 
+if($password != $password_confirm){
+    echo "passwords should be the same";
+}else if(mysqli_num_rows($query_check_info)==0){
+
+    $flag_check = true;
     if(mysqli_query($con, $sql)){
         echo "Registration Successful.";
     } else{echo "ERROR: Could not be able to execute".mysqli_error($con);}
+}else{
+    echo "invalid username, email or mobile number";
+}
 
  }
 
